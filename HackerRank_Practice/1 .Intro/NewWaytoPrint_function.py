6b28f06a1ee1104d06e65341960d6e0a4226eabd
Santoshkumar Yadav <syadav214@gmail.com>
2019-01-22 17:48:29 +0530

    example for print

--- HackerRank_Practice/1 .Intro/NewWaytoPrint_function.py
+++ HackerRank_Practice/1 .Intro/NewWaytoPrint_function.py
@@ -3,3 +3,9 @@ from __future__ import print_function
 if __name__ == '__main__':
     n = int(raw_input())
     print(*range(1,n+1),sep='')
+
+"""
+Example:>
+print(*values, sep=' ', end='\n', file=sys.stdout)
+print(value1, value2, value3, sep=' ', end='\n', file=sys.stdout)
+"""
