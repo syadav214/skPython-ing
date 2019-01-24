f99e1735eff88193770a72a6b6b288aaeba91935
Santoshkumar Yadav <syadav214@gmail.com>
2019-01-24 17:28:38 +0530

    changed a bit

--- HackerRank_Practice/1 .Intro/NewWaytoPrint_function.py
+++ HackerRank_Practice/1 .Intro/NewWaytoPrint_function.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 
 if __name__ == '__main__':
     n = int(raw_input())
-    print(*range(1,n+1),sep='')
+    print(*xrange(1,n+1),sep='',end='\nThis is the end\n')
+
 
 """
 Example:>
--- pyTest.py
+++ pyTest.py
@@ -1,5 +1,14 @@
-from __future__ import print_function
+A = input()
+A =  A.split(' ')
+x = int(A[0])
+y = int(A[1])
+z = int(A[2])
+n = int(A[3])
 
-if __name__ == '__main__':
-    n = int(raw_input())
-    print(*xrange(1,n+1),sep='')
+
+for a in range(x+1):
+    for b in range(y+1):
+        for c in range(z+1):
+            #print(a,b,c)
+            if a + b + c != n:
+                print(a,b,c)
